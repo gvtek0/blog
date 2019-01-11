5f86fb9bd551edd2479776d5e80e017ae63e31d1
gvtek0 <gvtek0@gmail.com>
2019-01-11 07:32:43 -0800

    Flask Blog First Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+# app.py
+import datetime
+import functools
+import os
+import re
+import urllib
+
+from flask import (Flask, abort, flash, Markup, redirect, render_template, request, Response, session, url_for)
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.splite_ext import *
+
+ADMIN_PASSWORD = "adminpassword"
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE = 'sqliteext:///%s' os.path.join(APP_DIR, 'blog.db')
+DEBUG = False
+SECRET_KEY = 'shhh, adminpassword!' # Used for Flask to encrypt session cookies
+SITE_WIDTH = 800
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+flask_db = FlaskDB(app)
+database = flask_db.database
+
+oembed_providers = bootstrap_basic(OEmbedCache())
